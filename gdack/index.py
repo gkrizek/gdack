5dddabbb7daee02a11956590eabaaefed49c7e46
Graham Krizek <gkrizek@nodesource.com>
2017-08-09 06:39:29 -0600

    update parameter store retrevial

--- gdack/index.py
+++ gdack/index.py
@@ -1,7 +1,20 @@
-
+import boto3
 
 def handler(event, context):
+
     if event:
+        print("Event Data:\n" + str(event))
+        # Get Secrets from SSM
+        ssm = boto3.client('ssm')
+        env = 'prod'
+        response = ssm.get_parameters_by_path(
+            Path='/gdack/' + env,
+            WithDecryption=True
+        )
+        for index, value in enumerate(response['Parameters']):
+            name = value['Name'].split('/')[-1]
+            os.environ[name] = value['Value']
+
         return {
             'statusCode': 200,
             'body': str(event)
